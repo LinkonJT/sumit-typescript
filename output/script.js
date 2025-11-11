803dbefaee548f2e94459349f9e66f6946808348
LinkonJT <linkontripura@gmail.com>
2025-11-11 18:31:01 +0600

    dynmic type

--- output/script.js
+++ output/script.js
@@ -1,4 +1,12 @@
-let fruits = ["mango", "apple", "banana"];
-fruits.push(4);
-console.log(fruits);
+// let fruits = ["mango", "apple", "banana"]
 export {};
+// fruits.push(4)
+// console.log(fruits)
+// let a: number[] = [];
+// a.push("john")  //error coz, variable is defined as number
+// let b: string | number;
+// // array
+// let a: (string | number)[] = []
+// b = "sumit"
+// a.push('sumit', 4, false) //false is red  cos bolean vlue not defined
+// Lesson 5 - Dynamic Type - TypeScript Bangla ( বাংলা ) Tutorial Series
--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,24 @@
-let fruits = ["mango", "apple", "banana"]
+// let fruits = ["mango", "apple", "banana"]
 
-fruits.push(4)
+// fruits.push(4)
 
-console.log(fruits)
+// console.log(fruits)
+
+
+// let a: number[] = [];
+
+// a.push("john")  //error coz, variable is defined as number
+
+// let b: string | number;
+
+
+// // array
+
+// let a: (string | number)[] = []
+
+// b = "sumit"
+
+// a.push('sumit', 4, false) //false is red  cos bolean vlue not defined
+
+
+// Lesson 5 - Dynamic Type - TypeScript Bangla ( বাংলা ) Tutorial Series
