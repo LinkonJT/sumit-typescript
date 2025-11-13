eaca3c20d3ac64959feacace30dbda7583e628af
LinkonJT <linkontripura@gmail.com>
2025-11-13 18:41:02 +0600

    function signatures

--- src/script.ts
+++ src/script.ts
@@ -49,16 +49,25 @@
 
 // Lesson 7 - Type Aliases 
 
-type stringOrNum= string | number
-type userType = {name: string; age: number}
+// type stringOrNum= string | number
+// type userType = {name: string; age: number}
 
-const userDetails = (
-    id: stringOrNum,
-    user: userType
-)=> {
-    console.log(`User id is ${id}, name is ${user.name} and age is ${user.age}`)
-}
+// const userDetails = (
+//     id: stringOrNum,
+//     user: userType
+// )=> {
+//     console.log(`User id is ${id}, name is ${user.name} and age is ${user.age}`)
+// }
 
-const sayHello = (user: userType)=> {
-    console.log(`Hello ${user.age > 50 ? "sir" : "Mr"} ${user.name}`)
+// const sayHello = (user: userType)=> {
+//     console.log(`Hello ${user.age > 50 ? "sir" : "Mr"} ${user.name}`)
+// }
+
+// Lesson 8 - Function Signatures 
+
+let add: (x; number, y: number) => number;
+
+add = (a: number, b: number) =>{
+    console.log(a+b)
+    return a+b;
 }
